f4a10e1eb6025ac0af1c79df5bb319e2269d700e
Chessin8tor <63427758+Chessin8tor@users.noreply.github.com>
2024-05-24 09:24:19 -0700

    Refactor client game code

--- ClientGameManager.java
+++ ClientGameManager.java
@@ -1,13 +1,72 @@
-import java.awt.Graphics;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.awt.event.*;
 
 public class ClientGameManager {
     private boolean inGame;
+    private int screenWidth;
+    private int screenHeight;
+    private Font exoRegular;
+    private String headerString;
+    private String username;
+    private JButton playButton;
+    private JButton helpButton;
+    private TextFieldWithPrompt nameInputTextField;
     private MyMap<String, Player> players;
+    // private MyMap<String, Tower> towers;
+    // private MyMap<String, Enemy> enemies;
+    // private MyMap<String, Bullet> bullets;
+    // private MyMap<String, Explosion> explosions;
 
     public ClientGameManager() {
+        inGame = false;
+        screenWidth = 800;
+        screenHeight = 600;
     }
 
-    public void initGame() {
+    public void initGame(ClientScreen screen) {
+        screen.setLayout(null);
+        screen.setDoubleBuffered(true);
+        screen.setPreferredSize(new Dimension(screenWidth, screenHeight));
+
+        try {
+            exoRegular = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Exo/static/Exo-Regular.ttf")).deriveFont(24f);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        headerString = "TOWER DEFENSE GAME";
+        username = "";
+
+        playButton = new JButton("PLAY");
+        playButton.setBounds(screenWidth / 2 - 50, screenHeight / 2 - 25, 100, 50);
+        playButton.setFont(exoRegular);
+        playButton.addActionListener(screen);
+
+        helpButton = new JButton("HELP");
+        helpButton.setBounds(screenWidth / 2 - 50, screenHeight / 2 + 50, 100, 50);
+        helpButton.setFont(exoRegular);
+        helpButton.addActionListener(screen);
+
+        nameInputTextField = new TextFieldWithPrompt();
+        nameInputTextField.setBounds(screenWidth / 2 - 50, screenHeight / 2 - 95, 100, 50);
+        nameInputTextField.setFont(exoRegular);
+        nameInputTextField.setForeground(Color.LIGHT_GRAY);
+        nameInputTextField.setOpaque(false);
+        nameInputTextField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        nameInputTextField.setCaretColor(Color.WHITE);
+        nameInputTextField.addActionListener(screen);
+        
+        screen.add(nameInputTextField);
+        screen.add(playButton);
+        screen.add(helpButton);
+
+        screen.addMouseListener(screen);
+        screen.addMouseMotionListener(screen);
+        screen.addKeyListener(screen);
+        screen.setFocusable(true);
     }
 
     public void startGame() {
@@ -17,11 +76,47 @@ public class ClientGameManager {
     }
 
     public void drawGame(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, screenWidth, screenHeight);
+
+        g.setColor(Color.WHITE);
+        g.setFont(exoRegular);
+
+        Graphics2D g2d = (Graphics2D) g;
+        FontMetrics fontMetrics = g2d.getFontMetrics();
+        int stringWidth = fontMetrics.stringWidth(headerString);
+
+        g.drawString(headerString, screenWidth/2 - stringWidth/2, 100);
+
+        if(nameInputTextField.isVisible()) {
+            g2d.setStroke(new BasicStroke(3));
+            g.setColor(Color.WHITE);
+            g.drawLine(screenWidth / 2 - 50, screenHeight / 2 - 50, screenWidth / 2 + 50, screenHeight / 2 - 50);
+        }
     }
 
-    public void handleInput(String input) {
+    public void handleInput(ActionEvent e) {
+        if(e.getSource() == playButton) {
+            username = nameInputTextField.getText();
+            nameInputTextField.setVisible(false);
+            playButton.setVisible(false);
+            helpButton.setVisible(false);
+            setHeaderString("WAITING FOR OTHER PLAYER");
+        }
     }
 
     public void handleMouseInput(int x, int y) {
+    }
+
+    public void setHeaderString(String headerString) {
+        this.headerString = headerString;
+    }
+
+    public JButton getPlayButton() {
+        return playButton;
+    }
+
+    public JButton getHelpButton() {
+        return helpButton;
     }
 }
--- ClientScreen.java
+++ ClientScreen.java
@@ -8,84 +8,17 @@ public class ClientScreen extends JPanel implements KeyListener, ActionListener,
     private Socket socket;
     private PrintWriter outputStream;
     private BufferedReader inputStream;
-    private JButton playButton;
-    private JButton helpButton;
-    private Font exoRegular;
-    private int screenWidth;
-    private int screenHeight;
-    private String headerString;
-    private String username;
     private ClientGameManager gameManager;
-    private TextFieldWithPrompt nameInputTextField;
 
     public ClientScreen() {
-        this.setLayout(null);
-        this.setDoubleBuffered(true);
-
-        screenWidth = 800;
-        screenHeight = 600;
-        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-
         gameManager = new ClientGameManager();
-
-        try {
-            exoRegular = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Exo/static/Exo-Regular.ttf")).deriveFont(24f);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        headerString = "TOWER DEFENSE GAME";
-        username = "";
-
-        playButton = new JButton("PLAY");
-        playButton.setBounds(screenWidth / 2 - 50, screenHeight / 2 - 25, 100, 50);
-        playButton.setFont(exoRegular);
-        playButton.addActionListener(this);
-
-        helpButton = new JButton("HELP");
-        helpButton.setBounds(screenWidth / 2 - 50, screenHeight / 2 + 50, 100, 50);
-        helpButton.setFont(exoRegular);
-        helpButton.addActionListener(this);
-
-        nameInputTextField = new TextFieldWithPrompt();
-        nameInputTextField.setBounds(screenWidth / 2 - 50, screenHeight / 2 - 95, 100, 50);
-        nameInputTextField.setFont(exoRegular);
-        nameInputTextField.setForeground(Color.LIGHT_GRAY);
-        nameInputTextField.setOpaque(false);
-        nameInputTextField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
-        nameInputTextField.setCaretColor(Color.WHITE);
-        nameInputTextField.addActionListener(this);
-        add(nameInputTextField);
-        add(playButton);
-        add(helpButton);
-
-        addMouseListener(this);
-        addMouseMotionListener(this);
-        this.addKeyListener(this);
-        this.setFocusable(true);
+        gameManager.initGame(this);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, screenWidth, screenHeight);
-
-        g.setColor(Color.WHITE);
-        g.setFont(exoRegular);
-
-        Graphics2D g2d = (Graphics2D) g;
-        FontMetrics fontMetrics = g2d.getFontMetrics();
-        int stringWidth = fontMetrics.stringWidth(headerString);
-
-        g.drawString(headerString, screenWidth/2 - stringWidth/2, 100);
-
-        if(nameInputTextField.isVisible()) {
-            g2d.setStroke(new BasicStroke(3));
-            g.setColor(Color.WHITE);
-            g.drawLine(screenWidth / 2 - 50, screenHeight / 2 - 50, screenWidth / 2 + 50, screenHeight / 2 - 50);
-        }
+        gameManager.drawGame(g);
     }
 
     public void connect() {
@@ -100,7 +33,7 @@ public class ClientScreen extends JPanel implements KeyListener, ActionListener,
                         String response = inputStream.readLine();
                         
                         if(response != null && response.equalsIgnoreCase("Start")) {
-                            headerString = "LOADING GAME";
+                            gameManager.setHeaderString("GAME STARTED");
                             repaint();
                         }
                     }
@@ -114,18 +47,10 @@ public class ClientScreen extends JPanel implements KeyListener, ActionListener,
         }
     }
 
-    public String username() {
-        return username;
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == playButton) {
-            username = nameInputTextField.getText();
-            nameInputTextField.setVisible(false);
-            playButton.setVisible(false);
-            helpButton.setVisible(false);
-            headerString = "WAITING FOR OTHER PLAYER";
+        gameManager.handleInput(e);
+        if (e.getSource() == gameManager.getPlayButton()) {
             try {
                 outputStream.println("play");
                 outputStream.flush();
@@ -134,7 +59,7 @@ public class ClientScreen extends JPanel implements KeyListener, ActionListener,
             }
             repaint();
         }
-        if (e.getSource() == helpButton) {
+        if (e.getSource() == gameManager.getHelpButton()) {
             System.out.println("Help");
         }
     }
